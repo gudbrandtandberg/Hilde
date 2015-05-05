50c0feee8eae7cbb607a56bf34ab4f56db0dec16
Gudbrand <gudbrandduff@gmail.com>
2015-05-05 23:28:41 +0200

    endringer paa server

---
+++ tmp.php
@@ -0,0 +1,108 @@
+<!-- Generelle tips:
+-Offer approval, return and refund policies
+-Provide clear concise instructions on how to buy
+-Provide adequate contact information
+-Think seriously about accompanying each series or body of your work with its own explanation or introduction.
+-Text explanations and introductions to your art are extremely important, but keep the word count to a minimum.
+-->
+
+<!-- NEWSFEED/HOME -->
+
+<?php
+    include("header.php");
+?>
+
+<div class="container">
+    <div class="row newscontainter">
+        
+        <div class="col-md-12 newsitem">
+            <h3>Welcome to hildemorris.com! <small>1.5.2015</small></h3>
+            
+        </div>
+        
+        <div class="col-md-12 newsitem">
+            <h3>Oslo Open <small>18.4.2015</small></h3>
+            
+            <p>
+                Oslo Open is an art festival where artists in Oslo open their studios to the public. This year more than 350 artists participated, including me!
+                I focused mainly on presenting my recent series of paintings from Sami reindeer herding and reindeer slaughter at a small local abattoir in Suossjavri in Finnmark.
+                There were many visitors, and Oslo Open was a great opportunity to show this project, which I have been working on since 2013.
+            </p>
+            
+            <table id="reintabell">
+                <tr>
+                    <td><a href="images/news/Skillegjerdemedutsikt.jpg"><img src="images/news/Skillegjerdemedutsikt.jpg"></a></td>
+                    <td><a href="images/news/guttenogreinen_skillegjerde.jpg"><img src="images/news/guttenogreinen_skillegjerde.jpg"></a></td>
+                </tr>
+                <tr>
+                    <td><a href="images/news/doedigjerdet_lores.jpg"><img src="images/news/doedigjerdet_lores.jpg"></a></td>
+                    <td><a href="images/news/avbloedning.jpg"><img src="images/news/avbloedning.jpg"></a></td>
+                </tr>
+                <tr>
+                    <td><a href="images/news/Minus30utenfor.jpg"><img src="images/news/Minus30utenfor.jpg"></a></td>
+                    <td><a href="images/news/nedpaabenken.jpg"><img src="images/news/nedpaabenken.jpg"></a></td>
+                </tr>
+                <tr>
+                    <td><a href="images/news/utavskinnet.jpg"><img src="images/news/utavskinnet.jpg"></a></td>
+                    <td><a href="images/news/utvomming.jpg"><img src="images/news/utvomming.jpg"></a></td>
+                </tr>
+                <tr>
+                    <td><a href="images/news/rensing.jpg"><img src="images/news/rensing.jpg"></a></td>
+                    <td><a href="images/news/TilHimmels.jpg"><img src="images/news/TilHimmels.jpg"></a></td>
+                </tr>
+            </table>
+            
+            
+        </div>
+        
+        
+        <div class="col-md-12 newsitem">
+            <h3>Dogs 4 All <small>16.11.2014</small></h3>
+            <p>Her er noen av skissene mine fra Dogs 4 All utstillingen 2014</p>
+            
+            <table>
+                <tr>
+                    <td><img src="images/news/Dogs4All0001.jpg" width="30%"></td>
+                    <td><img src="images/news/Dogs4All0002.jpg" width="30%"></td>
+                    <td><img src="images/news/Dogs4All0003.jpg" width="30%"></td>
+                </tr>
+                <tr>
+                    <td><img src="images/news/Dogs4All0004.jpg" width="30%"></td>
+                    <td><img src="images/news/Dogs4All0005.jpg" width="30%"></td>
+                    <td><img src="images/news/Dogs4All0006.jpg" width="30%"></td>
+                </tr>
+                <tr>
+                    <td><img src="images/news/Dogs4All0007.jpg" width="30%"></td>
+                    <td><img src="images/news/Dogs4All0009.jpg" width="30%"></td>
+                    <td><img src="images/news/Dogs4All0010.jpg" width="30%"></td>
+                </tr>
+                <tr>
+                    <td><img src="images/news/Dogs4All0011.jpg" width="30%"></td>
+                    <td><img src="images/news/Dogs4All0012.jpg" width="30%"></td>
+                    
+                </tr>
+            </table>
+      
+        </div>
+        
+        <!--<div class="col-md-12 newsitem">
+            
+            <h3>Jeg så et reinsdyr <small>11.1.2015</small></h3>
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </p>
+            
+            <div>
+                <img src="images/other/11.jpg" alt="bilde">
+                <p>Liten kommentar til bildet...</p>
+            </div>
+        </div>-->
+        
+        
+        
+    </div>
+</div>
+
+<?php
+    include("footer.php");
+?>
